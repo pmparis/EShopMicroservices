018ff53c6f65690890c73bdc0bb7a3e7f4e1efac
Phil Paris <pparis@rightpoint.com>
2025-01-13 22:20:04 -0600

    Adding Basket.API

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using Basket.API.Data;
+global using Basket.API.Models;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
+global using Carter;
+global using FluentValidation;
+global using Mapster;
+global using Marten;
+global using MediatR;
